d4da2add1765788a68841fef8c8e4f0c34530f79
Tammy L Leonard-Bruton <enigmalb@outlook.com>
2019-11-25 03:18:11 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  value_a = 0
+  value_a = nil
   name_hash.collect do |key, value|
 if value_a <=> value
     
