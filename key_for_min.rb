3e70ca8ff84045561517452aa2f63cfc550e15c7
Tammy L Leonard-Bruton <enigmalb@outlook.com>
2019-11-25 04:05:08 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -18,7 +18,16 @@ def key_for_min_value(name_hash)
 end
   
 
-
+ lowest_key = nil
+	  lowest_value = nil
+  hash.each do |k, v|	  hash.each do |k, v|
+    if v < lowest_value	    if lowest_value == nil || v < lowest_value
+      lowest_value = v	      lowest_value = v
+      lowest_key = k	      lowest_key = k
+    end	    end
+  end	  end
+  lowest_key	  lowest_key
+end 	end
 
 
 
